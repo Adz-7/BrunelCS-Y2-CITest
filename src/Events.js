9c8fe764952736f720f1cfec2484a616cf2baf2e
Adz-7 <73649002+Adz-7@users.noreply.github.com>
2021-03-11 11:11:06 +0000

    Added CSS to Buttons and Form/Still working on it

--- src/Events.js
+++ src/Events.js
@@ -16,7 +16,7 @@ export default class Events extends React.Component {
     this.state = {
         organisatoin: "",
         location: "",
-        fee: "",
+        details: "",
         date: "",
         time: "",
 
@@ -39,7 +39,7 @@ locationhandler = (event) => {
 }
 feehandler = (event) => {
     this.setState({
-        fee: event.target.value
+        details: event.target.value
     })
 }
 
@@ -61,8 +61,9 @@ handleSubmit = (event) => {
     this.setState({
         organisation: "",
         location: "",
-        fee: '',
+        details: '',
         date: "",
+        time: "",
     })
  event.preventDefault()
     
@@ -86,41 +87,43 @@ handleSubmit = (event) => {
             <EventsCard
             title = 'Swim Serpentine'
             imageUrl= 'https://images.unsplash.com/photo-1593113630400-ea4288922497?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            body = 'Swim Serpentine is a one-day open water swimming festival staged in the heart of London. Thousands of swimmers will participate in events that include. The One-Mile Swim takes place on the same course as the London 2012 Marathon Swimming Olympic event, with one full lap around the Serpentine making up the one-mile swim. All your needs are catered for with heated changing rooms, dunk zone, pontoon start and finish, full water safety crew, hot tubs and exhibition area.'
+            body = 'Swim Serpentine is a one-day open water swimming festival staged in the heart of London. Thousands of swimmers will participate in events that include. The One-Mile Swim takes place on the same course as the London 2021 Marathon Swimming Olympic event, with one full lap around the Serpentine making up the one-mile swim. All your needs are catered for with heated changing rooms, dunk zone, pontoon start and finish, full water safety crew, hot tubs and exhibition area.'
             button = "Learn More"
-            modaltext = "Some Text"
+            modaltext = "Location: London Date: Time:"
             />
             <EventsCard
              title = 'London to Brighton Cycle'
              imageUrl= 'https://images.pexels.com/photos/1125850/pexels-photo-1125850.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
              body = '“Cycle 55 miles starting from Clapham Common in South London, to the seaside city of Brighton. This ride will take you through some stunning countryside all the way down to the South coast. The registration cost of £55 per person includes a fully supported ride and a fantastic lunch at the half way point and a medal at the finish on the sea front in Brighton! Get in quick because the spaces in the race are filling up very fast.  '
              button = "Learn More"
-             modaltext = "Some Text"
+             modaltext = "Location: Clapham Common Date: Time:"
             />
             <EventsCard
              title = 'Three Peaks Challenge'
              imageUrl= 'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-             body = "The Yorkshire Three Peaks Challenge is a 12 hour trek that takes on the peaks of Pen-y-ghent, Whernside and Ingleborough. These peaks form part of the Pennine range, and encircle the head of the valley of the River Ribble, in the Yorkshire Dales National Park. The route is 24 miles (38.6km), and includes 1585m (5200ft) of ascent.Each year, there are several Yorkshire Three Peaks Challenge events which are open to all. Bookings are welcome for individuals and groups of any size and can be made through the Three Peaks website. Groups are usually about ten people, each with a mountain leader to guide and navigate as required. This event is booked and organised through The Three Peaks website."
+             body = "The Yorkshire Three Peaks Challenge is a 12 hour trek that takes on the peaks of Pen-y-ghent, Whernside and Ingleborough. These peaks form part of the Pennine range, and encircle the head of the valley of the River Ribble, in the Yorkshire Dales National Park. The route is 24 miles (38.6km), and includes 1585m of ascent. Bookings are welcome for individuals and groups of any size. Groups are usually about ten people, each with a mountain leader to guide and navigate as required."
              button = "Learn More"
-             modaltext = "Some Text"
+             modaltext = "Location: Yorkshire Date: Time:"
             />
         </div>
         <div>
 
+        <div className="box1">
 <form onSubmit={this.handleSubmit}>
-    <h1>Event Registration</h1>
+    <h1 className="titleDescription2">
+        Event Registration
+        </h1>
     <label>Organisation :</label> <input type="text" value={this.state.organisation} onChange={this.organisationthandler} placeholder="Organisation..." /><br />
     <label>Location :</label> <input type="text" value={this.state.location} onChange={this.locationhandler} placeholder="Location..." /><br />
-    <label>Fee :</label> <input type="fee" value={this.state.fee} onChange={this.feehandler} placeholder="Fee..." /><br />
+    <label>Details :</label> <input type="details" value={this.state.fee} onChange={this.detailshandler} placeholder="Details..." /><br />
     <label>Date :</label> <input type="date" value={this.state.Date} onChange={this.Datehandler} placeholder="Date" /><br />
-    <label>Time :</label> <input type="time" value={this.state.Time} onChange={this.Datehandler} placeholder="Time" /><br />
- 
-        
+    <label>Time :</label> <input type="time" value={this.state.Time} onChange={this.Datehandler} placeholder="Time" /><br />      
 <br />
     
     <input type="submit" value="Submit" />
 </form>
 
+</div>
 </div>
     
           </events>
--- src/components/EventsCardSct.js
+++ src/components/EventsCardSct.js
@@ -5,22 +5,25 @@ function EventsCardSct() {
     return (
         <div className="eventcardSct">
             <EventsCard
-            title = 'Why Volunteer?'
+            title = 'Swim Serpentine'
             imageUrl= 'https://images.unsplash.com/photo-1593113630400-ea4288922497?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            body = 'Become a volunteer and be the change you want to see in the world. Volunteering is crucial for our communities. It allows you to connect to your communitiy. Click learn more to why volunteering is important.'
+            body = 'Swim Serpentine is a one-day open water swimming festival staged in the heart of London. Thousands of swimmers will participate in events that include. The One-Mile Swim takes place on the same course as the London 2021 Marathon Swimming Olympic event, with one full lap around the Serpentine making up the one-mile swim. All your needs are catered for with heated changing rooms, dunk zone, pontoon start and finish, full water safety crew, hot tubs and exhibition area.'
             button = "Learn More"
+            modaltext = "Location: London Date: Time:"
             />
             <EventsCard
-             title = 'Donations'
+             title = 'London to Brighton Cycle'
              imageUrl= 'https://images.pexels.com/photos/1125850/pexels-photo-1125850.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-             body = '“Giving is not just about making a donation. It is about making a difference.” Kathy Calvin. Our mission is to make a difference by helping the poor children get the education they deserve. Click donate to make a change to a childs life.  '
+             body = '“Cycle 55 miles starting from Clapham Common in South London, to the seaside city of Brighton. This ride will take you through some stunning countryside all the way down to the South coast. The registration cost of £55 per person includes a fully supported ride and a fantastic lunch at the half way point and a medal at the finish on the sea front in Brighton! Get in quick because the spaces in the race are filling up very fast.  '
              button = "Learn More"
+             modaltext = "Location: Clapham Common Date: Time:"
             />
             <EventsCard
-             title = 'Statistics'
+             title = 'Three Peaks Challenge'
              imageUrl= 'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-             body = "Every child has the right to education. Millions of children are not learning because of poverty, discrimination and many more. Click learn more to find out more about how many children are unable to receive the education they deserve. "
+             body = "The Yorkshire Three Peaks Challenge is a 12 hour trek that takes on the peaks of Pen-y-ghent, Whernside and Ingleborough. These peaks form part of the Pennine range, and encircle the head of the valley of the River Ribble, in the Yorkshire Dales National Park. The route is 24 miles (38.6km), and includes 1585m of ascent. Bookings are welcome for individuals and groups of any size. Groups are usually about ten people, each with a mountain leader to guide and navigate as required."
              button = "Learn More"
+             modaltext = "Location: Yorkshire Date: Time:"
             />
         </div>
     )
