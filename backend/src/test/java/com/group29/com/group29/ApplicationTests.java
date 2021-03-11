b38a039704330c13a067c8136ece0a9ef204afdd
Adz-7 <73649002+Adz-7@users.noreply.github.com>
2021-03-11 11:28:52 +0000

    Changed Test to use embedded H2 database

--- backend/src/test/java/com/group29/com/group29/ApplicationTests.java
+++ backend/src/test/java/com/group29/com/group29/ApplicationTests.java
@@ -2,8 +2,11 @@ package com.group29.com.group29;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
